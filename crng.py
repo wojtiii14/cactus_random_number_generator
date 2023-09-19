f5be48cbb55ee426a1718a3562e8936e2442f330
Wojciech Kaczmarek <101423770+wojtiii14@users.noreply.github.com>
2023-09-19 20:09:38 +0200

    19.09 Update
    Entropy increased due to sbox implementation

--- crng.py
+++ crng.py
@@ -18,20 +18,39 @@ def entropy(data):
     
     return entropy
 
+with open('sbox_08x08_20130110_011319_02.SBX', 'rb') as  file:
+    sbox = file.read()
+    
+    counter = 0
+    all_bytes = []
+    
+    for byte in sbox:
+        if counter % 2 == 0:
+            all_bytes.append(byte)
+        counter += 1
+
 img = Image.open('cactus.jpg')
 pix = img.load()
 
 values = []
 
+counter = 0
+
 for x in range(img.size[0]):
     for y in range(img.size[1]):
+
         [r, g, b] = img.getpixel((x, y))
 
-        values.append(r)
-        values.append(g)
-        values.append(b)
+        if(counter == 255):
+            counter = 0
+        else:
+            counter = counter + 1
 
-x = int(input("How many numbers should be combined in random string: "))
+            values.append(r ^ all_bytes[counter])
+            values.append(g ^ all_bytes[counter])
+            values.append(b ^ all_bytes[counter])
+
+# x = int(input("How many numbers should be combined in random string: "))
 
 result = ""
 
@@ -41,9 +60,13 @@ for i in range(0, x):
 
     result = result + str(number)
 
-print(result)
+number = int(values[random.randint(0, len(values) - 1)])
+
+# print(result)
+
+print(number)
         
 # print(values)
 
 # ent = entropy(values)
-# print(f"Entropia wynosi: {ent}")
+# print(f"Entropy equals: {ent}")
